584ac930dc6d2f6eac6c8396e918c1497611e052
Inder <kumarinder799@gmail.com>
2026-01-05 15:29:46 +0530

    Task Page and Home page changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { images } from "./db/images";
+import { useBrowser } from "./context/browser-context";
+import { Home, Task } from "./pages/index.js";
+import { useEffect } from "react";
+
+const index = Math.floor(Math.random() * images.length);
+const bgImage = images[index].image;
 
 function App() {
+  const { name, browserDispatch } = useBrowser();
+
+  useEffect(() => {
+    const userName = localStorage.getItem("name");
+    browserDispatch({
+      type: "NAME",
+      payload: userName,
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" style={{ backgroundImage: `url("${bgImage}")` }}>
+      {/* if name ? <Task/> : <Home /> */}
+      {name ? <Task /> : <Home />}
     </div>
   );
 }
---
+++ src/db/quotes.js
@@ -0,0 +1,82 @@
+export const quotes = [
+  {
+    quote:
+      "You must be the change you wish to see in the world. - Mahatma Gandhi",
+  },
+  {
+    quote:
+      "The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
+  },
+  {
+    quote:
+      "Do what you can, with what you have, where you are. - Theodore Roosevelt",
+  },
+  {
+    quote:
+      "The best way to predict the future is to create it. - Peter Drucker",
+  },
+  { quote: "Believe you can and you're halfway there. - Theodore Roosevelt" },
+  {
+    quote:
+      "What you get by achieving your goals is not as important as what you become by achieving your goals. - Zig Ziglar",
+  },
+  { quote: "Dream big and dare to fail. - Norman Vaughan" },
+  { quote: "It always seems impossible until it’s done. - Nelson Mandela" },
+  {
+    quote:
+      "Happiness is not something ready made. It comes from your own actions. - Dalai Lama",
+  },
+  {
+    quote:
+      "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+  },
+  {
+    quote:
+      "Life is 10% what happens to us and 90% how we react to it. - Charles R. Swindoll",
+  },
+  {
+    quote:
+      "Do not go where the path may lead, go instead where there is no path and leave a trail. - Ralph Waldo Emerson",
+  },
+  {
+    quote: "Act as if what you do makes a difference. It does. - William James",
+  },
+  {
+    quote:
+      "Keep your face always toward the sunshine—and shadows will fall behind you. - Walt Whitman",
+  },
+  { quote: "Opportunities don't happen. You create them. - Chris Grosser" },
+  {
+    quote: "Don’t watch the clock; do what it does. Keep going. - Sam Levenson",
+  },
+  {
+    quote:
+      "Everything you’ve ever wanted is on the other side of fear. - George Addair",
+  },
+  {
+    quote:
+      "Hardships often prepare ordinary people for an extraordinary destiny. - C.S. Lewis",
+  },
+  {
+    quote:
+      "Success usually comes to those who are too busy to be looking for it. - Henry David Thoreau",
+  },
+  {
+    quote:
+      "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart. - Roy T. Bennett",
+  },
+  {
+    quote:
+      "You are never too old to set another goal or to dream a new dream. - C.S. Lewis",
+  },
+  {
+    quote:
+      "I find that the harder I work, the more luck I seem to have. - Thomas Jefferson",
+  },
+  { quote: "Keep going. Be all in. - Bryan Hutchinson" },
+  { quote: "Believe in yourself and all that you are. - Christian D. Larson" },
+  {
+    quote:
+      "Strive not to be a success, but rather to be of value. - Albert Einstein",
+  },
+];
